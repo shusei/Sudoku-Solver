3bae0b5b0191858711b768270640130f9aaa205f
heavener <shengjyun.lin@gmail.com>
2018-12-18 19:10:47 +0800

    Initial Commit

---
+++ Sudoku Solver/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Sudoku Solver
+//
+//  Created by SHENG CHUN LIN on 2018/12/18.
+//  Copyright © 2018 SHENG CHUN LIN. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
